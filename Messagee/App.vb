f48e84c3caec367ee86f8a486b20a049a2a0b5b1
Mike Malloy <mmalloy@apigee.com>
2013-09-30 20:49:32 -0700

    Initial commit of V1
    Requires creation of an admin user for the org and some sample app
    users.  Future revisions will add creation of new users.

---
+++ Messagee/App.vb
@@ -0,0 +1,22 @@
+﻿Imports Usergrid.Sdk
+Imports Usergrid.Sdk.Model
+
+Public Class App
+
+    Public Function GetFollowers(userName As String) As IList
+        Dim conn As Connection = New Connection()
+        conn.ConnectorIdentifier = userName
+        conn.ConnectorCollectionName = "users"
+        conn.ConnectionName = "following"
+        Return Globals.client.GetConnections(conn)
+    End Function
+
+    Public Function GetFollowed(userName As String) As IList
+        Dim conn As Connection = New Connection()
+        conn.ConnectorIdentifier = userName
+        conn.ConnectorCollectionName = "users"
+        conn.ConnectionName = "followed"
+        Return Globals.client.GetConnections(conn)
+    End Function
+
+End Class
